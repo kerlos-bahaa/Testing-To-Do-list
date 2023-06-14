622b8af61116b7c18228cc780d511e2935b9065b
kerlosbahaa3 <aboelking4@gmail.com>
2023-06-14 16:22:10 +0300

    Test2

--- src/tests/todo.test.js
+++ src/tests/todo.test.js
@@ -1,13 +1,19 @@
-import addTask from './__mock__/todo.js';
+import addTask from "./__mock__/todo.js";
 
-describe('addTask', () => {
-  it('should add a new task to the list', () => {
+describe("addTask", () => {
+  it("should add a new task to the list", () => {
     // addTask('Buy groceries');
-    expect(addTask('Buy groceries')).toEqual([
+    expect(addTask("Buy groceries")).toEqual([
       {
-        description: 'Buy groceries',
+        description: "Buy groceries",
         completed: false,
         index: 0,
+      },
+    ]);
+  });
+  it("should remove a task from the list", () => {
+    expect(deleteTask("Buy groceries")).toEqual([
+      {
       },
     ]);
   });
